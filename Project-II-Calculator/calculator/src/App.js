178c4bda93628b6ed29b03e1f01d2f6b27858174
Manju KC <Manju KC>
2019-04-23 18:33:57 -0400

    getting numberbutton

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay"
+import NumberButton from "./components/ButtonComponents/NumberButton"
 
 const App = () => {
   return(
+    <div>
     <div className = "display" >
       < CalculatorDisplay text ={"0"}/>
+    </div>
+    <div>
+      <NumberButton  text={"0"}/>
+      <NumberButton  text={"1"}/>
+      <NumberButton text = {"2"}/>
+      <NumberButton text = {"3"}/>
+      <NumberButton text = {"4"}/>
+      <NumberButton text = {"5"}/>
+      <NumberButton text = {"6"}/>
+      <NumberButton text = {"7"}/>
+      <NumberButton text = {"8"}/>
+      <NumberButton text = {"9"}/>
+    </div>
     </div>
   )
   
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Button.css';
  function NumberButton(props){
-     const number = [1,2,3,4,5,6,7];
+  
     return(
       <div>
-          <button className={props.buttonStyle}>{props.numberProp.number}</button>
+          <button className={props.buttonStyle}>{props.text}</button>
       </div>
     )
  }
