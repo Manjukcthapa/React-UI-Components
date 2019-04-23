42da6fce5366e593ad0268c4cf497a4e66e14d23
Manju KC <Manju KC>
2019-04-23 18:03:49 -0400

    got a display button and working on button

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+ function NumberButton(props){
+     const number = [1,2,3,4,5,6,7];
+    return(
+      <div>
+          <button className={props.buttonStyle}>{props.numberProp.number}</button>
+      </div>
+    )
+ }
+
+ export default NumberButton;
